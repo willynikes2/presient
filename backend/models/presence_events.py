f616bf287e48bb738c0de2420aab25195808b1e8
willynikes2 <presientio@gmail.com>
2025-05-22 17:50:57 +0000

    commit

--- backend/models/presence_events.py
+++ backend/models/presence_events.py
@@ -2,11 +2,11 @@
 from sqlalchemy import Column, String, Float, DateTime
 from backend.db.base import Base
 import uuid
-import datetime
+from datetime import datetime
 
 class PresenceEvent(Base):
     __tablename__ = "presence_events"
-
+    
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
     user_id = Column(String, nullable=False)
     sensor_id = Column(String, nullable=False)
