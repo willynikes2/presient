01e4e46be8ea23c0abc0a7d559d3f8dcd855efd0
willynikes2 <presientio@gmail.com>
2025-05-22 19:21:13 +0000

    commit

--- backend/services/mqtt.py
+++ backend/services/mqtt.py
@@ -2,8 +2,4 @@ class MockMQTTClient:
     def publish(self, topic, payload):
         print(f"MQTT Publish: {topic} → {payload}")
 
-<<<<<<< HEAD
 mqtt_client = MockMQTTClient()
-=======
-mqtt_client = MockMQTTClient()
->>>>>>> d0aee7c15f003d5e9a37838a3fcd9bf4b258c668
