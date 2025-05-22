ac227b9c3c98e1c2e58bd5a2abfcf744c3593163
willynikes2 <presientio@gmail.com>
2025-05-22 19:05:32 +0000

    commit

--- backend/db/session.py
+++ backend/db/session.py
@@ -7,3 +7,13 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+from sqlalchemy.orm import Session
+from fastapi import Depends
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
